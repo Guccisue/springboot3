071cad5614bd3faecbfb0617096eb7101b55be87
Guccisue <527008445@qq.com>
2020-04-15 16:53:02 +0800

    Initial commit

---
+++ src/main/java/cn/cjy/test/redisTest.java
@@ -0,0 +1,22 @@
+package cn.cjy.test;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class redisTest {
+
+    @Before
+    public void before(){
+
+    }
+
+    @Test
+    public void test(){
+
+    }
+}
