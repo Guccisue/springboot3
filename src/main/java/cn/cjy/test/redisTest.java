4236c77033dea1077ab0a5fe61f1e99869fda103
Guccisue <527008445@qq.com>
2020-04-15 18:30:31 +0800

    12121

--- src/main/java/cn/cjy/test/redisTest.java
+++ src/main/java/cn/cjy/test/redisTest.java
@@ -5,18 +5,22 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class redisTest {
 
+    private Jedis jedis;
+
     @Before
     public void before(){
-
+        jedis = new Jedis("39.96.28.20",6379);
     }
 
     @Test
     public void test(){
+        System.out.println(jedis.select(4));
 
     }
 }
